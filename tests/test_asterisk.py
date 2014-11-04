add2192302ee99daccd48bdf535ea573f6e83062
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 17:43:53 +0100

    Migrate to version 3.4

--- tests/test_asterisk.py
+++ tests/test_asterisk.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
-#This file is part asterisk module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part asterisk module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class AsteriskTestCase(unittest.TestCase):
-    '''
-    Test Asterisk module.
-    '''
+    'Test Asterisk module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('asterisk')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('asterisk')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
